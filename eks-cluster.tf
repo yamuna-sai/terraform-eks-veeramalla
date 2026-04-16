3e9e5661a034426392152cc84b25e8373b7116bf
yamuna-sai <yamunat1805@gmail.com>
2026-04-16 13:50:19 +0530

    Update eks-cluster.tf

--- eks-cluster.tf
+++ eks-cluster.tf
@@ -1,7 +1,7 @@
 module "eks" {
   source          = "terraform-aws-modules/eks/aws"
   version         = "20.8.4"
-  cluster_name    = local.cluster_name
+  cluster_name    = yamuna
   cluster_version = var.kubernetes_version
   subnet_ids      = module.vpc.private_subnets
 
